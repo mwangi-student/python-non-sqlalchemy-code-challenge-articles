34f49491103c20e9865a66865f72203d62175e28
chieby-mwangi <chiebyrah@gmail.com>
2024-12-08 23:28:47 +0300

    code-challenge

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,102 @@
-class Article:
-    def __init__(self, author, magazine, title):
-        self.author = author
-        self.magazine = magazine
-        self.title = title
-        
 class Author:
     def __init__(self, name):
-        self.name = name
+        self._name = name
+        self._articles = []
 
+    @property
+    def name(self):
+        return self._name
+
+    @property
     def articles(self):
-        pass
+        return self._articles
 
+    @property
     def magazines(self):
-        pass
+        return list({article.magazine for article in self._articles})
 
     def add_article(self, magazine, title):
-        pass
+        article = Article(self, magazine, title)
+        self._articles.append(article)
+        return article
 
     def topic_areas(self):
-        pass
+        return list({magazine.category for magazine in self.magazines})
+
 
 class Magazine:
+    all = []
+
     def __init__(self, name, category):
-        self.name = name
-        self.category = category
+        if not (2 <= len(name) <= 16):
+            raise ValueError("Magazine name must be between 2 and 16 characters.")
+        if not category:
+            raise ValueError("Category cannot be empty.")
+        self._name = name
+        self._category = category
+        self._articles = []
+        Magazine.all.append(self)
 
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        if not (2 <= len(new_name) <= 16):
+            raise ValueError("Magazine name must be between 2 and 16 characters.")
+        self._name = new_name
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, new_category):
+        if not new_category:
+            raise ValueError("Category cannot be empty.")
+        self._category = new_category
+
+    @property
     def articles(self):
-        pass
+        return self._articles
 
+    @property
     def contributors(self):
-        pass
-
-    def article_titles(self):
-        pass
+        return list({article.author for article in self._articles})
 
     def contributing_authors(self):
-        pass
+        return [author for author in self.contributors if len([a for a in self._articles if a.author == author]) > 2]
+
+    @classmethod
+    def top_publisher(cls):
+        return max(cls.all, key=lambda magazine: len(magazine.articles), default=None)
+
+
+class Article:
+    all = []  
+    def __init__(self, author, magazine, title):
+        # Initialize author, magazine, and title attributes
+        self._author = author
+        self._magazine = magazine
+        self.title = title
+        Article.all.append(self)  # Add the current instance to the class-level all list
+
+    # Author property and setter to allow mutation
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, new_author):
+        self._author = new_author
+
+    # Magazine property and setter to allow mutation
+    @property
+    def magazine(self):
+        return self._magazine
+
+    @magazine.setter
+    def magazine(self, new_magazine):
+        self._magazine = new_magazine
+
--- lib/testing/article_test.py
+++ lib/testing/article_test.py
@@ -1,5 +1,4 @@
 import pytest
-
 from classes.many_to_many import Article
 from classes.many_to_many import Magazine
 from classes.many_to_many import Author
@@ -24,15 +23,10 @@ class TestArticle:
         magazine = Magazine("Vogue", "Fashion")
         article_1 = Article(author, magazine, "How to wear a tutu with style")
 
-        # comment out the next two lines if using Exceptions
-        article_1.title = 500
-        assert article_1.title == "How to wear a tutu with style"
-        
         assert isinstance(article_1.title, str)
 
-        # uncomment the next two lines if using Exceptions
-        # with pytest.raises(Exception):
-        #     Article(author, magazine, 500)
+        with pytest.raises(Exception):
+            Article(author, magazine, 500)
 
     def test_title_is_valid(self):
         """title is between 5 and 50 characters inclusive"""
@@ -42,13 +36,11 @@ class TestArticle:
 
         assert 5 <= len(article_1.title) <= 50
 
-        # uncomment the next two lines if using Exceptions
-        # with pytest.raises(Exception):
-        #     Article(author, magazine, "Test")
+        with pytest.raises(Exception):
+            Article(author, magazine, "Test")
 
-        # uncomment the next two lines if using Exceptions
-        # with pytest.raises(Exception):
-        #     Article(author, magazine, "How to wear a tutu with style and walk confidently down the street")
+        with pytest.raises(Exception):
+            Article(author, magazine, "How to wear a tutu with style and walk confidently down the street")
 
     def test_has_an_author(self):
         """article has an author"""
@@ -104,7 +96,7 @@ class TestArticle:
 
     def test_get_all_articles(self):
         """Article class has all attribute"""
-        Article.all = []
+        Article.all = []  # Ensure this is reset before test
         author = Author("Carry Bradshaw")
         magazine_1 = Magazine("Vogue", "Fashion")
         magazine_2 = Magazine("AD", "Architecture & Design")
